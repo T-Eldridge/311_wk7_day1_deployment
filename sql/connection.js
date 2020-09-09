ff772108618647a0544d769c97a736b8f1db612b
TEldridge <skippydrumskins@yahoo.com>
2020-09-09 09:35:09 -0500

    Deployment

--- sql/connection.js
+++ sql/connection.js
@@ -1,16 +1,18 @@
 const mysql = require('mysql')
 
+require('dotenv').config()
+
 class Connection {
   constructor() {
     if (!this.pool) {
       console.log('creating mysql connection...')
 
       const config = {
-        connectionLimit: 100,
-        host: 'localhost',
-        user: 'root',
-        password: 'password',
-        database: 'admin'
+       host: process.env.HOST,
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        database: process.env.DATABASE
+
       }
 
       if (process.env.NODE_ENV === 'production' && process.env.CLOUD_INSTANCE) {
